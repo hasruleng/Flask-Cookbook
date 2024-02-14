a5a88abd1901ca14aa64dd14abec9e0f40a1742f
hasruleng <hasrulwho@gmail.com>
2024-02-14 23:14:43 +0100

    alt. web forms using FlaskForm

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
 from flask import Flask, render_template, request
-from helper import recipes, descriptions, ingredients, instructions, add_ingredients, add_instructions
+from helper import recipes, descriptions, ingredients, instructions, add_ingredients, add_instructions, comments
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
 
+#### Create form class here
+class CommentForm(FlaskForm):
+  comment = StringField("Comment")
+  submit = SubmitField("Add Comment")
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
-  new_id = len(recipes) + 1
-  if len(request.form) > 0:
-    #### Add the recipe name to recipes[new_id]
-    recipes[new_id] = request.form["recipe"]
-        #### Add the recipe description to descriptions[new_id]
-    descriptions[new_id] = request.form["description"]
-    #### Add the values to new_ingredients and new_instructions
-    new_ingredients = request.form["ingredients"]
-    new_instructions = request.form["instructions"]
-    add_ingredients(new_id, new_ingredients)
-    add_instructions(new_id, new_instructions)
   return render_template("index.html", template_recipes=recipes)
 
-@app.route("/recipe/<int:id>")
+@app.route("/recipe/<int:id>", methods=["GET", "POST"])
 def recipe(id):
-  return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id])
+  #### Instantiate form class here
+  comment_form = CommentForm()
+  return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id], template_comments=comments[id], template_form=comment_form)
+
+@app.route('/about')
+def about():
+  return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
